53e7625632888efb6c3f42be1cbd5c04708a06ce
Rebeka-1892 <etu1892@gmail.com>
2023-11-21 17:52:05 +0300

    Xfcqd

--- src/main/java/nba/stats/EquipeController.java
+++ src/main/java/nba/stats/EquipeController.java
@@ -7,6 +7,7 @@ import java.util.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/equipes")
 public class EquipeController {
--- src/main/java/nba/stats/PossessionController.java
+++ src/main/java/nba/stats/PossessionController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/possessions")
 public class PossessionController {
