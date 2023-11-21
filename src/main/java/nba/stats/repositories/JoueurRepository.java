f7c177d289d8c39726f7ea74428737ac2f9bb27d
Rebeka-1892 <etu1892@gmail.com>
2023-11-21 12:30:41 +0300

    nofafana

--- src/main/java/nba/stats/repositories/JoueurRepository.java
+++
@@ -1,8 +0,0 @@
-package main.java.nba.stats.repositories;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import main.java.nba.stats.models.Joueur;
-@Repository
-public interface JoueurRepository extends JpaRepository<Joueur, Integer>{
-    
-}
